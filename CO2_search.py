74f55ac869c1326f62602431434e6c5a34a638c4
sahasragayathri02 <sahasrach.0226@gmail.com>
2026-05-31 20:57:31 +0530

    Update CO2_search.py

--- CO2_search.py
+++ CO2_search.py
@@ -1,40 +1,118 @@
-# CO3 - Constraint Satisfaction
+from collections import deque
+import heapq
 
-GRID = [
-    [0,0,0],
-    [0,1,0],
-    [0,0,0]
-]
+graph = {
+    'A': {'B':1,'C':4},
+    'B': {'D':2,'E':5},
+    'C': {'F':1},
+    'D': {},
+    'E': {'G':2},
+    'F': {'G':3},
+    'G': {}
+}
 
-sensor_observation = (1,1)
+# BFS
+def bfs(start, goal):
 
-candidates = []
+    queue = deque([[start]])
+    visited = set()
 
-for i in range(3):
-    for j in range(3):
+    while queue:
 
-        if GRID[i][j] == 0:
-            candidates.append((i,j))
+        path = queue.popleft()
+        node = path[-1]
 
-def satisfies_constraint(cell):
+        if node == goal:
+            return path
 
-    x,y = cell
+        if node not in visited:
 
-    return (x+y)%2 == 0
+            visited.add(node)
 
-def backtracking(candidates):
+            for neighbor in graph[node]:
+                queue.append(path+[neighbor])
 
-    solutions = []
+# DFS
+def dfs(start, goal):
 
-    for cell in candidates:
+    stack = [[start]]
+    visited = set()
 
-        if satisfies_constraint(cell):
-            solutions.append(cell)
+    while stack:
 
-    return solutions
+        path = stack.pop()
+        node = path[-1]
 
-print("\nPossible Positions:")
-print(candidates)
+        if node == goal:
+            return path
+
+        if node not in visited:
+
+            visited.add(node)
+
+            for neighbor in graph[node]:
+                stack.append(path+[neighbor])
+
+# UCS
+def ucs(start, goal):
+
+    pq = [(0,start,[start])]
+
+    while pq:
+
+        cost,node,path = heapq.heappop(pq)
+
+        if node == goal:
+            return path,cost
+
+        for neighbor,w in graph[node].items():
+
+            heapq.heappush(
+                pq,
+                (cost+w,
+                 neighbor,
+                 path+[neighbor])
+            )
+
+# A*
+heuristic = {
+    'A':6,
+    'B':4,
+    'C':4,
+    'D':3,
+    'E':2,
+    'F':2,
+    'G':0
+}
+
+def astar(start,goal):
+
+    pq = [(0,start,[start],0)]
+
+    while pq:
+
+        f,node,path,g = heapq.heappop(pq)
+
+        if node == goal:
+            return path,g
+
+        for neighbor,w in graph[node].items():
+
+            new_g = g+w
+            new_f = new_g+heuristic[neighbor]
+
+            heapq.heappush(
+                pq,
+                (new_f,
+                 neighbor,
+                 path+[neighbor],
+                 new_g)
+            )
+
+print("\nBFS:", bfs('A','G'))
+print("DFS:", dfs('A','G'))
+print("UCS:", ucs('A','G'))
+print("A* :", astar('A','G'))print(candidates)
 
 print("\nValid Positions After CSP:")
 print(backtracking(candidates))
