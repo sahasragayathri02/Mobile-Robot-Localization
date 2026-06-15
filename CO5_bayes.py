9f24d46d9df2b10553ecad5a3613139198f8de95
sahasragayathri02 <sahasrach.0226@gmail.com>
2026-06-15 20:33:21 +0530

    Update CO5_bayes.py

--- CO5_bayes.py
+++ CO5_bayes.py
@@ -1,44 +1,22 @@
+# ===============================
 # CO5 - Bayes Filter
+# ===============================
 
-belief = {
-    "A":0.25,
-    "B":0.25,
-    "C":0.25,
-    "D":0.25
-}
+belief = {"A":0.25,"B":0.25,"C":0.25,"D":0.25}
+likelihood = {"A":0.1,"B":0.9,"C":0.6,"D":0.2}
 
-likelihood = {
-    "A":0.1,
-    "B":0.9,
-    "C":0.6,
-    "D":0.2
-}
-
-print("\nInitial Belief")
-print(belief)
+print("\n--- CO5: BAYES FILTER ---")
+print("Initial Belief:", belief)
 
 new_belief = {}
-
 total = 0
 
-for state in belief:
+for s in belief:
+    new_belief[s] = belief[s] * likelihood[s]
+    total += new_belief[s]
 
-    new_belief[state] = (
-        belief[state] *
-        likelihood[state]
-    )
+for s in new_belief:
+    new_belief[s] /= total
 
-    total += new_belief[state]
-
-for state in new_belief:
-    new_belief[state] /= total
-
-print("\nUpdated Belief")
-print(new_belief)
-
-most_likely = max(
-    new_belief,
-    key=new_belief.get
-)
-
-print("\nMost Likely State:",most_likely)
+print("Updated Belief:", new_belief)
+print("Most Likely:", max(new_belief, key=new_belief.get))
