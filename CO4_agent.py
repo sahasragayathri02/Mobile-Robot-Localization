f44bd28c96253d50aa027723ae29b4eae43fa343
sahasragayathri02 <sahasrach.0226@gmail.com>
2026-06-15 20:32:50 +0530

    Update CO4_agent.py

--- CO4_agent.py
+++ CO4_agent.py
@@ -1,42 +1,27 @@
+# ===============================
 # CO4 - Decision Agent
+# ===============================
 
 actions = {
-    "FORWARD": {
-        "info_gain":5,
-        "cost":1
-    },
-
-    "TURN_LEFT":{
-        "info_gain":3,
-        "cost":0.5
-    },
-
-    "TURN_RIGHT":{
-        "info_gain":2,
-        "cost":0.5
-    }
+    "FORWARD": {"info_gain":5, "cost":1},
+    "TURN_LEFT":{"info_gain":3, "cost":0.5},
+    "TURN_RIGHT":{"info_gain":2, "cost":0.5}
 }
 
-def utility(info_gain,cost):
-
-    return info_gain - 0.2*cost
+def utility(info_gain, cost):
+    return info_gain - 0.2 * cost
 
 best_action = None
 best_score = -999
 
-print("\nAction Utilities")
+print("\n--- CO4: DECISION AGENT ---")
 
-for action,data in actions.items():
-
-    score = utility(
-        data["info_gain"],
-        data["cost"]
-    )
-
-    print(action,"=",score)
+for a, data in actions.items():
+    score = utility(data["info_gain"], data["cost"])
+    print(a, "=>", score)
 
     if score > best_score:
         best_score = score
-        best_action = action
+        best_action = a
 
-print("\nSelected Action:",best_action)
+print("Best Action:", best_action)
